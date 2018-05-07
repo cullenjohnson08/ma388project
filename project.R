436f8ed019dc5bcb9ef4907dd573bdb35ade0faf
cullenjohnson08 <cullen.johnson@usma.edu>
2018-05-07 01:10:29 -0400

    added model development

--- project.R
+++ project.R
@@ -10,6 +10,7 @@ batters = read_csv("Batting.csv")
 
 head(master)
 
+# Input and select desired data from the Master.csv
 master = 
   master %>%
   select(playerID, nameFirst, nameLast, nameGiven, weight, height, bats, throws, debut, finalGame)
@@ -21,26 +22,54 @@ master =
 
 head(master)
 
+
+# Input and select desired data from the Batting.csv
 batters =
   batters %>% group_by(playerID) %>%
-  summarise(cG = sum(G), cAB = sum(AB), cR = sum(R), cH = sum(H), cRBI = sum(RBI), cSB = sum(SB), cSO = sum(SO), LastYear = max(yearID))
+  summarise(cG = sum(G), cAB = sum(AB), cR = sum(R), cH = sum(H), cRBI = sum(RBI), cSB = sum(SB), cSO = sum(SO), LastYear = max(yearID), totalSeasons = sum(!is.na(playerID)))
 
 head(batters)
 
+
+# join the master and batters to create data
 data = master %>% left_join(batters)
 
 head(data)
 
+# trim out bad datapoints 
 data = data %>%
-  filter(!is.na(weight), !is.na(height), !is.na(bats), !is.na(throws), !is.na(debut), !is.na(finalGame), !is.na(cG), !is.na(cAB), !is.na(cAB),
+  filter(!is.na(weight), !is.na(height), !is.na(bats), !is.na(throws), !is.na(debut), !is.na(finalGame), !is.na(cG), !is.na(cAB),
          !is.na(cR), !is.na(cH), !is.na(cRBI), !is.na(cSB), !is.na(cSO), !is.na(LastYear))
 
+# add a boolean (one or zero) whether the players last year is 2016, meaning they are alive, alive being a 0
 data = data %>%
-  mutate(isIn = ifelse(LastYear > 2015, 1, 0))
+  mutate(isIn = ifelse(LastYear > 2015, 0, 1))
 
+# seperate for inspection purposes
 playersIn = data %>%
-  filter(isIn == 1)
-
-playersOut = data %>%
   filter(isIn == 0)
 
+playersOut = data %>%
+  filter(isIn == 1)
+
+# build the object to start our model based on the players total seasons and whether or not they are "alive"
+object = Surv(data$totalSeasons, data$isIn)
+object
+
+# build out kaplan meier object
+km = survfit(object~1)
+summary(km)
+
+plot(km, conf.int = TRUE)
+
+#first model - note P-Values for batsL, throwsS, and cR are all high
+cox1 = coxph(object~ weight + height + bats + throws + cAB + cR + cH + cRBI + cSB + cSO, data = data)
+summary(cox1)
+plot(survfit(cox1), conf.int = TRUE)
+
+#trim out cR
+
+# second model - note P-Values for batsL and throwsS are both high
+cox2 = coxph(object~ weight + height + bats + throws + cAB + cH + cRBI + cSB + cSO, data = data)
+summary(cox2)
+plot(survfit(cox2), conf.int = TRUE)
